4efca44429facf8a46b2e51641693c5cb549e99a
Kocann <kocela.ann@gmail.com>
2018-05-02 16:18:22 +0200

    refractoring

--- server/config/config.js
+++ server/config/config.js
@@ -1,17 +1,27 @@
 var env = process.env.NODE_ENV || 'development';
 
-let db = {
-  mlab: 'mongodb://ann:ann@ds241869.mlab.com:41869/todoappapp',
-  localhost: 'mongodb://localhost:27017/TodoApp',
-  localhostTest: 'mongodb://localhost:27017/TodoAppTEST'
+if (env === 'development' || env === 'test') {
+  var config = require('./config.json');
+  var envConfig = config[env];
+
+  Object.keys(envConfig).forEach(key => {
+    process.env[key] = envConfig[key]
+  })
 }
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = db.localhost;
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI = db.localhostTest;
-} else {
-  process.env.MONGODB_URI = db.mlab;
-}
+
+// let db = {
+//   mlab: 'mongodb://ann:ann@ds241869.mlab.com:41869/todoappapp',
+//   localhost: 'mongodb://localhost:27017/TodoApp',
+//   localhostTest: 'mongodb://localhost:27017/TodoAppTEST'
+// }
+
+// if (env === 'development') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = db.localhost;
+// } else if (env === 'test') {
+//   process.env.PORT = 3000;
+//   process.env.MONGODB_URI = db.localhostTest;
+// } else {
+//   process.env.MONGODB_URI = db.mlab;
+//}
